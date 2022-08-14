96b057dbb2ba4f1fdf13c6ad6a761147f574da60
Dominik Zogg <dominik.zogg@hotmail.com>
2022-08-14 10:05:45 +0200

    do not listen to output to known if the server is ready but by calling the server

--- jest.integration.global-setup.js
+++ jest.integration.global-setup.js
@@ -1,4 +1,5 @@
 const { spawn } = require('child_process');
+const fetch = require('cross-fetch');
 
 const mongoDbSetup = require('@shelf/jest-mongodb/setup');
 
@@ -6,36 +7,42 @@ const getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
 const testServerHost = '127.0.0.1';
 const testServerPort = getRandomInt(49152, 65535);
 
+const timeout = 10000;
+const iterationTimeout = 500;
+
 const startServer = async () => {
-  return new Promise((resolve, reject) => {
-    const child = spawn(process.argv[0], ['node_modules/.bin/ts-node', 'bootstrap/index.ts'], {
-      env: {
-        NODE_ENV: 'jest',
-        MONGO_URI: process.env.MONGO_URI,
-        SERVER_HOST: testServerHost,
-        SERVER_PORT: testServerPort,
-      },
-      detached: true,
-    }).once('error', (err) => {
-      console.error(err);
-      reject();
-    });
-
-    let data = '';
-
-    child.stdout.on('data', function (buffer) {
-      data += buffer.toString('utf-8');
-
-      if (-1 !== data.search('Listening to')) {
-        resolve(child);
-      }
-    });
+  const child = spawn(process.argv[0], ['node_modules/.bin/ts-node', 'bootstrap/index.ts'], {
+    env: {
+      NODE_ENV: 'jest',
+      MONGO_URI: process.env.MONGO_URI,
+      SERVER_HOST: testServerHost,
+      SERVER_PORT: testServerPort,
+    },
+    detached: true,
+  }).once('error', (e) => {
+    throw e;
   });
+
+  for (let i = timeout; i > 0; i -= iterationTimeout) {
+    try {
+      await fetch(`http://${testServerHost}:${testServerPort}`);
+      return child;
+    } catch (e) {
+      if (e.code === 'ECONNREFUSED') {
+        console.log('wait for test server to be up and running...');
+        await new Promise((resolve) => setTimeout(resolve, iterationTimeout));
+      } else {
+        throw e;
+      }
+    }
+  }
+
+  throw new Error(`Timeout in starting the server`);
 };
 
 module.exports = async () => {

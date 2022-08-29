b2ed1bd98c3abc9be5f62b27300f92a816a9b18c
Dominik Zogg <dominik.zogg@hotmail.com>
2022-08-29 18:21:38 +0200

    use the version for ubuntu 20.04

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -6,10 +6,10 @@ const version = '5.0.11'
 if (os.platform() === 'linux') {
   // eslint-disable-next-line no-console
   console.log(
-    'WARNING: Monkey patching mongoms download URL for debian 10 in our docker builds!',
+    'WARNING: Monkey patching mongoms download URL for ubuntu 20.04 in our docker builds!',
   );
   process.env.MONGOMS_DOWNLOAD_URL =
-    `https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-debian10-${version}.tgz`;
+    `https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-${version}.tgz`;
 }
 
 module.exports = {
